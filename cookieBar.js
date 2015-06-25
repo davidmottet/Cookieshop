8e52738459d63226290b70848d8af1e350830c8b
mottet <mottet.tlse@gmail.com>
2015-06-25 17:20:18 +0200

    Update cookieBar.js

--- cookieBar.js
+++ cookieBar.js
@@ -15,12 +15,12 @@
 		//text
 		frosting = "En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies conformément à notre politique de données personnelles.",
 		//bar
-		ingerdient = "background: #161616; position: fixed; width: 100%; color: #ffffff; font-family: Helvetica, Arial, sans-serif; z-index: 1510; font-size: 12px; cursor: pointer; margin: 0 auto; height: 30px; line-height: 30px; text-align: center; top: 0; left: 0; right: 0;",
+		ingerdient = "background: #161616; position: fixed; width: 100%; color: #ffffff; font-family: Helvetica, Arial, sans-serif; z-index: 1510; font-size: 12px; cursor: pointer; margin: 0 auto; min-height: 20px; line-height: 20px; text-align: center; top: 0; left: 0; right: 0;",
 		//button
-		chip_bottel = "position: absolute; top: 0; bottom: 0; right: 20px; width: 20px; height: 2px; background-color: #ffffff; margin: auto;",
+		chip_bottel = "position: absolute; bottom: 9px; right: 20px; width: 20px; height: 2px; background-color: #ffffff; margin: auto;",
 		//cross
 		spreadBlack = "-webkit-transform: rotate(45deg); -moz-transform: rotate(45deg); -o-transform: rotate(45deg); -ms-transform: rotate(45deg); transform: rotate(45deg);",
-		spreadWhite = "-webkit-transform: rotate(90deg); -moz-transform: rotate(90deg); -o-transform: rotate(90deg); -ms-transform: rotate(90deg); transform: rotate(90deg); right: 0;",
+		spreadWhite = "-webkit-transform: rotate(90deg); -moz-transform: rotate(90deg); -o-transform: rotate(90deg); -ms-transform: rotate(90deg); transform: rotate(90deg); right: 0; bottom: 0;",
 
 	init = function () {
 		var brownie = tasteCookie(flavor);
@@ -52,12 +52,11 @@
 	},
 
 	showCookies = function () {
-		var oven = document.createElement("div");
-		oven.id = "cookies";
-		oven.style.cssText = ingerdient;
 
 		var cookie = document.createElement("div");
 		cookie.textContent = frosting;
+		cookie.id = "cookies";
+		cookie.style.cssText = ingerdient;
 
 		var chipBlack = document.createElement("span");
 		chipBlack.style.cssText = chip_bottel+spreadBlack;
@@ -68,8 +67,7 @@
 		var table = document.getElementsByTagName("body");
 		chipBlack.appendChild(chipWhite);
 		cookie.appendChild(chipBlack);
-		oven.appendChild(cookie);
-		table[0].appendChild(oven);
+		table[0].appendChild(cookie);
 
 		oven.addEventListener("click", function () {
 			table[0].removeChild(oven);
@@ -82,5 +80,3 @@
 	window.cookieShop = cookieShop;
 	return cookieShop;
 })(this);
-
-window.cookieShop.init();
